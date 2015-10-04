0a69bc5d8949fd055c39238fae1639de14326e1c
kaiyes <kaiyes.ansary@gmail.com>
2015-10-04 13:12:27 +0600

    includes all the form optionsg

--- practise/practise.js
+++ practise/practise.js
@@ -123,6 +123,92 @@ Biye.attachSchema(new SimpleSchema({
      allowedValues: ['Male', 'Female']
   },
 
+  age: {
+      type: Number,
+      max: 60      
+  },
+
+  profession: {
+      type: String      
+  },
+
+  currentResidence: {
+      type: String      
+  },
+
+  originalResidence : {
+      type: String      
+  },
+
+  familyMembers: {
+      type: String     
+  },
+
+  lastOrNextDegree: {
+     type: String,
+     allowedValues: ['Phd','Masters','Honours','Diploma','HSC','SSC']
+  },
+
+  dateOfBirth: {
+     type: Date,
+  },
+
+  religiousHistory: {
+     type: String,
+     allowedValues: ['Started practising 1 or 2 years back', 
+                      'Been practising for more than 5 years',
+                      'Revert Muslim','Have not started practising yet but want too soon',
+                      'I have faith in my heart']
+  },
+
+  sect: {
+     type: String,
+     allowedValues: ['Salafi/Ahle Hadeeth', 'Hanafi',
+                      'Tablig','Pir','Shia','I do not know']
+  },
+
+  prayer: {
+     type: String,
+     allowedValues: ['Always pray', 'Sometimes miss fajr',
+                      'Often pray','Before exam, I pray',
+                       'Eid only','Jumuah Only',
+                       'Will start praying very soon']
+  },
+
+  maritalStatus: {
+     type: String,
+     allowedValues: ['Never Married', 'Annulled (Khula)',
+                    'Divorced','widowed','Married']
+  },
+
+  numberOfChildren: {
+     type: Number   
+  },
+
+  Hijab: {
+     type: String,
+     allowedValues: ['Always With Burkha/Abaya face open', 
+                     'Always with Burkha/Abaya with Niqab',
+                     'Always with Scraf only',
+                     'I dress modestly but not Burkha/Abaya/niqab',
+                     'I dress modestly but Sometimes I wear Hijab',
+                     '(Male only) Shirt,trousers,casual clothing',
+                     '(Male only) Panjabi','(Male only) Jobba']
+  },
+
+  height: {
+     type: String   
+  },
+
+  beard:{
+    type: String,
+
+    allowedValues: ['I have let my beard grow','I trim my beard', 
+                    'No beaerd, shaved, will not keep beard',
+                    'No beaerd, shaved, will keep beard in future',
+                    'I am a woman, I CAN NOT HAVE BEARD you silly']
+  },
+
   image: {
     type: String,
     autoform: {
