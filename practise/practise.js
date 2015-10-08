f3aa329327a7418a31944b34e714a9b539d1a990
kaiyes <kaiyes.ansary@gmail.com>
2015-10-08 10:28:29 +0600

    adds chat

--- practise/practise.js
+++ practise/practise.js
@@ -1,5 +1,5 @@
 Biye = new Meteor.Collection ('biye');
-
+ChatRooms = new Meteor.Collection("chatrooms");
 
 if(Meteor.isClient) {
   
@@ -33,8 +33,10 @@ AutoForm.setDefaultTemplate('materialize');
          var email = event.target.email.value;
          var password = event.target.password.value;
          var gender = event.target.sex.value; 
+         var name = event.target.username.value; 
          profile = {
-            gender: gender
+            gender: gender,
+            name:name
           }; 
          Accounts.createUser({            
                email: email,
@@ -96,6 +98,8 @@ AutoForm.setDefaultTemplate('materialize');
              return Biye.find({gender: "Male"});
             
          }; 
+
+        
       }
     });
 
@@ -105,7 +109,13 @@ AutoForm.setDefaultTemplate('materialize');
            event.preventDefault();
            Meteor.logout();
            Router.go("home");
+         },
+
+         'click [name=chat]': function () {
+           event.preventDefault();
+           Router.go('/chat');
          }
+
        });
 
 
@@ -114,100 +124,16 @@ AutoForm.setDefaultTemplate('materialize');
 
 Biye.attachSchema(new SimpleSchema({
   
-  userName: {
-      type: String      
-  },
+  // userName: {
+  //     type: String      
+  // },
 
   gender: {
      type: String,
      allowedValues: ['Male', 'Female']
   },
 
-  age: {
-      type: Number,
-      max: 60      
-  },
-
-  profession: {
-      type: String      
-  },
-
-  currentResidence: {
-      type: String      
-  },
-
-  originalResidence : {
-      type: String      
-  },
-
-  familyMembers: {
-      type: String     
-  },
-
-  lastOrNextDegree: {
-     type: String,
-     allowedValues: ['Phd','Masters','Honours','Diploma','HSC','SSC']
-  },
-
-  dateOfBirth: {
-     type: Date,
-  },
-
-  religiousHistory: {
-     type: String,
-     allowedValues: ['Started practising 1 or 2 years back', 
-                      'Been practising for more than 5 years',
-                      'Revert Muslim','Have not started practising yet but want too soon',
-                      'I have faith in my heart']
-  },
-
-  sect: {
-     type: String,
-     allowedValues: ['Salafi/Ahle Hadeeth', 'Hanafi',
-                      'Tablig','Pir','Shia','I do not know']
-  },
-
-  prayer: {
-     type: String,
-     allowedValues: ['Always pray', 'Sometimes miss fajr',
-                      'Often pray','Before exam, I pray',
-                       'Eid only','Jumuah Only',
-                       'Will start praying very soon']
-  },
-
-  maritalStatus: {
-     type: String,
-     allowedValues: ['Never Married', 'Annulled (Khula)',
-                    'Divorced','widowed','Married']
-  },
-
-  numberOfChildren: {
-     type: Number   
-  },
-
-  Hijab: {
-     type: String,
-     allowedValues: ['Always With Burkha/Abaya face open', 
-                     'Always with Burkha/Abaya with Niqab',
-                     'Always with Scraf only',
-                     'I dress modestly but not Burkha/Abaya/niqab',
-                     'I dress modestly but Sometimes I wear Hijab',
-                     '(Male only) Shirt,trousers,casual clothing',
-                     '(Male only) Panjabi','(Male only) Jobba']
-  },
-
-  height: {
-     type: String   
-  },
-
-  beard:{
-    type: String,
-
-    allowedValues: ['I have let my beard grow','I trim my beard', 
-                    'No beaerd, shaved, will not keep beard',
-                    'No beaerd, shaved, will keep beard in future',
-                    'I am a woman, I CAN NOT HAVE BEARD you silly']
-  },
+  
 
   image: {
     type: String,
@@ -243,8 +169,153 @@ AutoForm.addHooks('details',{
        }
      });
 
-}
+// .....................Chat .............
+
+
+Template.sidebar.helpers({
+    'onlusr':function(){
+        return Meteor.users.find({_id:{$ne:Meteor.userId()}});
+    }
+});
+
+Template.sidebar.events({
+    'click [name=user]':function(){
+        Session.set('currentId',this._id);
+        var get = Session.get('currentId');
+        var res=ChatRooms.findOne({chatIds:{$all:[this._id,Meteor.userId()]}});
+        if(res)
+        {
+            //already room exists
+            Session.set(  "roomid",res._id);
+        }
+        else{
+            //no room exists
+            var newRoom= ChatRooms.insert({chatIds:[this._id , Meteor.userId()],messages:[]});
+            Session.set('roomid',newRoom);
+        }
+
+    }
+});
+
+Template.messages.helpers({
+    'msgs':function(){
+        var result=ChatRooms.findOne({_id:Session.get('roomid')});
+        
+       return result.messages;
+    }
+});
+
+Template.input.events({
+  'keyup [name=message]' : function (event) {
+    if (event.which == 13) { 
+       if (Meteor.user()) {
+          var name = Meteor.user().profile.name;
+          var message =  $('[name="message"]').val();
+
+              if(message !== ''){ 
+                ChatRooms.update({"_id":Session.get("roomid")},
+                  {$push:
+                    {messages:{name: name,text: message,createdAt: Date.now()}
+                  }
+                });    
+                $('[name="message"]').val('');    
+              } //if message
+        } // if user
+    } // if enter
+  } //keyup
+});
+        
+
+}   // meteor is client ...............................
 
 if(Meteor.isServer){
   
+
 }
+
+
+// age: {
+  //     type: Number,
+  //     max: 60      
+  // },
+
+  // profession: {
+  //     type: String      
+  // },
+
+  // currentResidence: {
+  //     type: String      
+  // },
+
+  // originalResidence : {
+  //     type: String      
+  // },
+
+  // familyMembers: {
+  //     type: String     
+  // },
+
+  // lastOrNextDegree: {
+  //    type: String,
+  //    allowedValues: ['Phd','Doctor','Engineer','Masters','Honours','Diploma','HSC','SSC']
+  // },
+
+  // dateOfBirth: {
+  //    type: Date,
+  // },
+
+  // religiousHistory: {
+  //    type: String,
+  //    allowedValues: ['Started practising 1 or 2 years back', 
+  //                     'Been practising for more than 5 years',
+  //                     'Revert Muslim','Have not started practising yet but want too soon',
+  //                     'I have faith in my heart']
+  // },
+
+  // sect: {
+  //    type: String,
+  //    allowedValues: ['Salafi/Ahle Hadeeth', 'Hanafi',
+  //                     'Tablig','Pir','Shia','I do not know']
+  // },
+
+  // prayer: {
+  //    type: String,
+  //    allowedValues: ['Always pray', 'Sometimes miss fajr',
+  //                     'Often pray','Before exam, I pray',
+  //                      'Eid only','Jumuah Only',
+  //                      'Will start praying very soon']
+  // },
+
+  // maritalStatus: {
+  //    type: String,
+  //    allowedValues: ['Never Married', 'Annulled (Khula)',
+  //                   'Divorced','widowed','Married']
+  // },
+
+  // numberOfChildren: {
+  //    type: Number   
+  // },
+
+  // Hijab: {
+  //    type: String,
+  //    allowedValues: ['Always With Burkha/Abaya face open', 
+  //                    'Always with Burkha/Abaya with Niqab',
+  //                    'Always with Scraf only',
+  //                    'I dress modestly but not Burkha/Abaya/niqab',
+  //                    'I dress modestly but Sometimes I wear Hijab',
+  //                    '(Male only) Shirt,trousers,casual clothing',
+  //                    '(Male only) Panjabi','(Male only) Jobba']
+  // },
+
+  // height: {
+  //    type: String   
+  // },
+
+  // beard:{
+  //   type: String,
+
+  //   allowedValues: ['I have let my beard grow','I trim my beard', 
+  //                   'No beaerd, shaved, will not keep beard',
+  //                   'No beaerd, shaved, will keep beard in future',
+  //                   'I am a woman, I CAN NOT HAVE BEARD you silly']
+  // },
