5a5bb0f5db2ed7a7b896634331816ec7f5ca4ee0
x <x@xs-iMac.local>
2017-01-25 20:33:38 +0600

    finished new details page, updating meteor users remain

--- source/client/components/Details Form/onRendered_Details_Routing.js
+++ source/client/components/Details Form/onRendered_Details_Routing.js
@@ -19,3 +19,30 @@ Template.updateDetails.helpers({
      return Biye.findOne({createdBy: Meteor.userId()});
   }
 });
+
+Template.details.events({
+
+   'submit form': function() {
+      event.preventDefault();
+      var name = event.target.name.value;
+      var gender = event.target.gender.value;
+      var height = event.target.height.value;
+      var profession = event.target.profession.value;
+      var education = event.target.education.value;
+      var prayer = event.target.prayer.value;
+      var practising = event.target.practising.value;
+      var beard = event.target.beard.value;
+      var hijab = event.target.hijab.value;
+      var aboutMe = event.target.aboutMe.value;
+
+      profile = {
+        // hasDb:false,
+        // approved:false,
+        // banned:false,
+        name, gender,height, profession, education, prayer, practising, beard, hijab,aboutMe
+      };
+
+      console.log(profile);
+
+    },
+});
--- source/client/components/forgotPassword/forgotPassword.js
+++ source/client/components/forgotPassword/forgotPassword.js
@@ -1,23 +1,23 @@
 Template.forgotPassword.events({
   'submit form': function(e,t) {
-  e.preventDefault();
-  var forgotPassword = $(e.currentTarget);
-  var email = forgotPassword.find('#email').val().toLowerCase();
-  console.log(email);
+    e.preventDefault();
+    var forgotPassword = $(e.currentTarget);
+    var email = forgotPassword.find('#email').val().toLowerCase();
+    console.log(email);
 
-  if (!email=="") {
-    Accounts.forgotPassword({email: email}, function(err) {
-      if (err) {
-        if (err.message === 'User not found [403]') {
-          sweetAlert('This email does not exist.');
+    if (!email=="") {
+      Accounts.forgotPassword({email: email}, function(err) {
+        if (err) {
+          if (err.message === 'User not found [403]') {
+            sweetAlert('This email does not exist.');
+          } else {
+            sweetAlert('We are sorry but something went wrong.');
+          }
         } else {
-          sweetAlert('We are sorry but something went wrong.');
+          sweetAlert('Email Sent. Check your mailbox.');
         }
-      } else {
-        sweetAlert('Email Sent. Check your mailbox.');
-      }
-    });
+      });
+     }
+     Router.go("/");
    }
-   Router.go("/");
- }
 });
